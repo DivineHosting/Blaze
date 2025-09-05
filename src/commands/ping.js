d931525d6f77b0dd9e9ec4a96401fdece805e3ea
Synero <anmolkumar48011@gmail.com>
2025-09-05 13:25:24 +0530

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,7 +1,7 @@
-export const name = "ping";
-export const description = "Show bot latency";
-export async function execute(message) {
-  const sent = await message.channel.send("Pinging...");
-  const latency = sent.createdTimestamp - message.createdTimestamp;
-  await sent.edit(`Pong! Latency: ${latency}ms`);
-}
+export default {
+  name: "ping",
+  description: "Replies with Pong!",
+  async execute(message) {
+    await message.reply("🏓 Pong!");
+  }
+};

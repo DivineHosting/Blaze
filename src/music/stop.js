e7aec461bbcc94645d8a3c36d77f31fa18b867bc
DivineHosting <anmolkumar48011@gmail.com>
2025-09-05 11:31:03 +0530

    Delete src/music/stop.js

--- src/music/stop.js
+++
@@ -1,8 +0,0 @@
-export const name = "stop";
-export const description = "Stop playback and clear queue";
-
-export async function execute(message, args, client) {
-  const ok = client.musicManager.stop(message.guild.id);
-  if (ok) await message.channel.send('⏹ Stopped and cleared queue.');
-  else await message.channel.send('Nothing was playing.');
-}
